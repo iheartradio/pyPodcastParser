cdc6c07b9358a67d6390037c90aa42c466fabd5f
Ian Noyes <ian.noyes@iheartmedia_com>
2023-11-14 14:37:00 -0600

    invalid start dates include seconds

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pypodcastparser-ihr',
 
-    version='1.9.0',
+    version='1.10.0',
 
     description='pypodcastparser is a podcast parser.',
     long_description=long_description,
