0c037885ee07d99068acf43f8d7a7f85959917aa
Drew Josey <drewjosey@Admins-MacBook-Pro.local>
2024-01-31 15:24:20 -0600

    exception now sends blank downstream

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
     ],
 
     install_requires=[
