4032b9603505d69ffed58f31377fc30e31c28114
Drew Josey <drewjosey@Admins-MacBook-Pro.local>
2024-09-03 11:59:02 -0500

    renaming

--- setup.py
+++ setup.py
@@ -6,45 +6,31 @@ import os
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
-released_version = os.environ.get('VERSION')
+released_version = "1.9.2timezonesdev"
 
 setup(
-    name='pypodcastparser-ihr',
-
+    name="pypodcastparser-ihr",
     version=released_version,
-
-    description='pypodcastparser is a podcast parser.',
+    description="pypodcastparser is a podcast parser.",
     long_description=long_description,
-
-    url='https://github.com/iheartradio/pypodcastparser',
-
-    author='Christian Paul, Jason Rigden',
-    author_email='christianpaul@iheartmedia.com, jasonrigden@gmail.com',
-
-    license='MIT',
-
+    url="https://github.com/iheartradio/pypodcastparser",
+    author="Christian Paul, Jason Rigden",
+    author_email="christianpaul@iheartmedia.com, jasonrigden@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup :: XML',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup :: XML",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
     ],
-
     install_requires=[
         "beautifulsoup4",
         "lxml",
     ],
-
-    keywords=['podcast', 'parser', 'rss', 'feed'],
-
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-
+    keywords=["podcast", "parser", "rss", "feed"],
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
 )
